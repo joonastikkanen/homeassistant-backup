2151c387ff7b1a949a71f336b48dbf786422ff50
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-09-03 22:37:33 +0300

    update backup create function

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Python script to backup Homeassistant to remote
 ```yaml
 ssh_user: "root"
 ssh_password: "password"
-ssh_port: 22
 ssh_host: ""
+ssh_port: 22
+homeassistant_url: "http://homeassistant.joonastikkanen.fi"
+homeassistant_token: "token"
 backup_dir: "/backup"
 remote_backup_dir: "/backup"
 ```
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import paramiko
 import yaml
+import requests
 
 # LOAD CONFIG FILE
 def load_config():
@@ -15,19 +16,34 @@ ssh_user = config['ssh_user']
 ssh_password = config['ssh_password']
 ssh_host = config['ssh_host']
 ssh_port = config['ssh_port']
+homeassistant_url = config['homeassistant_url']
+homeassistant_token = config['homeassistant_token']
 backup_dir = config.get('backup_dir', '/backup')  # Default backup directory
 remote_backup_dir = config.get('remote_backup_dir', '/remote/backup/path')  # Default remote backup directory
 # Create a backup of Home Assistant
 def create_backup():
-    backup_command = ['ha', 'snapshots', 'new', '--name', 'Automated Backup']
-    result = subprocess.run(backup_command, capture_output=True, text=True)
-    if result.returncode != 0:
-        raise Exception(f"Backup creation failed: {result.stderr}")
-    print("Backup created successfully.")
-    return result.stdout.strip()
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer %s' % homeassistant_token
+    }
+    data = {}
+    response = requests.post(f"http://%s/api/services/backup/create" % homeassistant_url, headers=headers, json=data)
+    if response.status_code == 200:
+        print("Backup created successfully.")
+        return True
+    else:
+        print("Failed to create backup.")
+        return None
+
+def get_backup_file():
+    backup_files = os.listdir(backup_dir)
+    newest_file = max(backup_files, key=os.path.getctime)
+    print(f"Newest backup file: {newest_file}")
+    return newest_file
 
 # Upload the backup file to the server
 def upload_backup(backup_file):
+    backup_file = get_backup_file()
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(ssh_host, port=ssh_port, username=ssh_user, password=ssh_password)
@@ -50,3 +66,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+api/services/backup/create
